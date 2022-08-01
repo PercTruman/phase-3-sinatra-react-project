ef02e91c14b904daeb6985e6fecb7d27105285e1
PercTruman <drshawnhart@gmail.com>
2022-08-01 10:10:32 -0500

    changed some of the controllers setup

--- app/controllers/army_controller.rb
+++ app/controllers/army_controller.rb
@@ -6,19 +6,41 @@ class ArmyController < ApplicationController
 
     
 
-    get '/army/:id' do
+    get '/armies/:id' do
         army = Army.find(params[:id])
         army.to_json(include: :army_models)
     end
 
 
-    # post '/army/:id' do
-    #     army = Army.create(
-    #         name: params[:name],
-    #         alignment: params[:alignment],
-    #         description: params[:description]
-    #     )
-    #     army.to_json
-    # end
+    get '/armies' do
+        armies = Army.all
+        armies.to_json(include: :army_models)
+      end
+    
+    
+    
+      post '/add_new_army'  do
+        army = Army.create(
+          name:params[:name],
+          alignment:params[:alignment],
+          description:params[:description]
+        )
+        army.to_json
+      end
+    
+      delete '/armies/:id' do
+        army= Army.find(params[:id])
+        army.destroy
+        army.to_json
+      end
+
+      patch '/armies/:id' do
+        army= Army.find(params[:id])
+        army.update(
+          number_in_collection: params[:number_in_collection],
+          unit_points_cost: params[:unit_points_cost]
+        )
+        army.to_json
+      end
 
 end
--- app/controllers/army_models_controller.rb
+++ app/controllers/army_models_controller.rb
@@ -1,33 +1,39 @@
 class ArmyModelsController < ApplicationController
   
-    get '/all_models_list/' do
-        models = ArmyModel.all
-        models.to_json
+    # get '/all_models' do
+    #     models = ArmyModel.all
+    #     models.to_json
+    # end
+
+    post '/add_new_models' do
+        models = ArmyModel.create(
+            name: params[:name],
+            image_url: params[:image_url],
+            number_in_collection: params[:number_in_collection],
+            cost_per_box: params[:cost_per_box],
+            unit_points_cost: params[:unit_points_cost],
+            army_id: params[:army_id]
+        )
+      models.to_json  
+    end
+
+    patch '/armies/:id' do
+        model = ArmyModel.find(params[:id])
+        model.update(
+            
+            number_in_collection: params[:number_in_collection],
+            unit_points_cost: params[:unit_points_cost]
+         )
+         model.to_json
     end
 
 
-
-    # if army_models.save
-    #     army_models.to_json
-    # else
-    #     { errors: army_models.errors.full_messages }.to_json
-    # end
-
-    delete '/all_models_list/:id' do
+    delete '/all_models/:id' do
         model = ArmyModel.find(params[:id])
         model.destroy
         model.to_json
     end
 
-    patch '/all_models_list/:id' do
-        model = ArmyModel.find(params[:id])
-        model.update(
-            name: params[:name],
-            number_in_collection: params[:number_in_collection],
-            cost_per_box: params[:cost_per_box],
-            unit_points_cost: params[:unit_points_cost]
-         )
-         model.to_json
-    end
+
 
 end
